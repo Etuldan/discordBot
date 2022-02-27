dca56259c7aa1cdc7d47f095914a4b18d750d533
Loupi06 <crosm06@gmail.com>
2022-02-27 13:31:44 +0100

    [FIX] Test test test

--- bot.py
+++ bot.py
@@ -399,13 +399,13 @@ class Bot(discord.Client):
         await temp.send(" - Enseignement")
         await temp.send(" - Spéciale")   
     
-    async def AddRDV(self, patient, phone, category, reason, medecine, medic):
+    async def AddRDV(self, patient, phone, category, medecine, reason, medic):
         embedVar = discord.Embed(color=COLOR_GREEN)
         embedVar.set_author(name="Prise de RDV", icon_url=IMG_LSMS_SCEAU)
         embedVar.add_field(name="Patient", value=patient, inline=True)
         embedVar.add_field(name="Téléphone", value=phone, inline=True)
+        embedVar.add_field(name="Médecine Générale", value=medecine, inline=True)         
         embedVar.add_field(name="Raison", value=reason, inline=False)
-        embedVar.add_field(name="Médecine Générale", value=medecine, inline=False)        
         embedVar.set_footer(text=medic.display_name)        
         if(category == 1):
             await self.channelRDVPsy.send(embed=embedVar)
